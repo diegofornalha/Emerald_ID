01e00616f56d19e515f2139fa78f31c9be9a9d56
Diego Fornalha <diegofornalha@gmail.com>
2023-10-10 00:50:41 -0300

    pt br

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,16 @@ import { useFlow } from '../context/FlowContext';
 import Link from 'next/link';
 
 function Home(props) {
-  const { desautenticar } = useFlow();
+  const { unauthenticate } = useFlow();
 
   useEffect(() => {
-    desautenticar();
+    unauthenticate();
   }, []);
 
   return (
     <div className={styles.app}>
       <main>
-        <h1>Emerald <span style={{ color: '#fff' }}>ID</span></h1>
+      <h1>Emerald <span style={{ color: '#fff' }}>ID</span></h1>
         <p>Vincule o seu <span style={{ color: '#5865F2' }}>Discord</span> a todas as suas carteiras <span style={{ color: '#16ff99' }}>Flow</span>, para que você só precise verificar a sua carteira uma vez.</p>
         <div className={styles.margin10}>
           <Link href="/me">
